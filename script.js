d22c3aa72cfd99856e4cc7f450181b21b093f436
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-05-16 22:54:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// let obj = () => {
+//   let obj = {
+//     project: (document.getElementById('project-dzaky').style.display = 'block'),
+//     academic: document.getElementById('academic-dzaky').style,
+//     competition: document.getElementById('competition-dzaky').style,
+//     community: document.getElementById('community-dzaky').style,
+//   };
+//   return obj;
+// };
+
+function project() {
+  document.getElementById('project-dzaky').style.display = 'block';
+  document.getElementById('academic-dzaky').style.display = 'none';
+  document.getElementById('competition-dzaky').style.display = 'none';
+  document.getElementById('community-dzaky').style.display = 'none';
+}
+
+function academic() {
+  document.getElementById('project-dzaky').style.display = 'none';
+  document.getElementById('academic-dzaky').style.display = 'block';
+  document.getElementById('competition-dzaky').style.display = 'none';
+  document.getElementById('community-dzaky').style.display = 'none';
+}
+
+function competition() {
+  document.getElementById('project-dzaky').style.display = 'none';
+  document.getElementById('academic-dzaky').style.display = 'none';
+  document.getElementById('competition-dzaky').style.display = 'block';
+  document.getElementById('community-dzaky').style.display = 'none';
+}
+
+function community() {
+  document.getElementById('project-dzaky').style.display = 'none';
+  document.getElementById('academic-dzaky').style.display = 'none';
+  document.getElementById('competition-dzaky').style.display = 'none';
+  document.getElementById('community-dzaky').style.display = 'block';
+}
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         // sticky navbar on scroll script
+//         if(this.scrollY > 20){
+//             $('.myFirstNav').addClass("sticky");
+//         }else{
+//             $('.myFirstNav').removeClass("sticky");
+//         }
